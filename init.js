a39da882b2ee0ccf5861cd6a9be12f9476f1eed1
Andrea Baccega <vekexasia@gmail.com>
2014-01-14 17:56:06 +0100

    Added Sophisticated logging interface. Added package.json which contains also a dependency

--- init.js
+++ init.js
@@ -1,21 +1,27 @@
-var fs = require('fs');
+var fs         = require('fs');
+var dateFormat = require('dateformat');
+var Stratum    = require('stratum-pool');
+var PoolLogger = require('./libs/logutils.js');
 
-var Stratum = require('stratum-pool');
+var loggerInstance = new PoolLogger({
+    'default': true,
+    'keys': {
+        'client'      : 'warning',
+        'system'      : true,
+        'submitblock' : true,
+    }
+});
 
-
-
-var timeLog = function(text, poolName){
-    var desc = poolName ? '[' + poolName + '] ' : '';
-    var time = new Date().toISOString();
-    console.log(time + ': ' + desc + text);
-};
+var logDebug   = loggerInstance.logDebug;
+var logWarning = loggerInstance.logWarning;
+var logError   = loggerInstance.logError;
 
 var config = JSON.parse(fs.readFileSync("config.json"));
 
 
 var stratum = new Stratum(config);
 stratum.on('log', function(logText){
-    timeLog(logText);
+    logDebug(logText);
 });
 
 
@@ -25,7 +31,7 @@ fs.readdirSync('coins').forEach(function(file){
 
     var authorizeFN = function (ip, workerName, password, callback) {
         // Default implementation just returns true
-        timeLog(coinOptions.name, "Authorize ["+ip+"] "+workerName+":"+password);
+        logDebug(coinOptions.name, 'client', "Authorize ["+ip+"] "+workerName+":"+password);
         callback({
             error: null,
             authorized: true,
@@ -37,12 +43,18 @@ fs.readdirSync('coins').forEach(function(file){
     var pool = stratum.createPool(coinOptions, authorizeFN);
     pool.on('share', function(isValid, data){
         if (isValid)
-            timeLog(coinOptions.name, "A new Valid share from " + data.client.workerName + " has arrived! - " + data.headerHex);
+            logDebug(coinOptions.name, 'client',  "A new Valid share from " + data.client.workerName + " has arrived! - " + data.blockHeaderHex);
         else
-            timeLog(coinOptions.name, "Invalid share form " + data.client.workerName + " ErrorCode: " + data.errorCode + " ErrorDescription: " + data.errorDescription);
-    }).on('log', function(logText){
-            timeLog(coinOptions.name, logText);
-        });
+            logDebug(coinOptions.name, 'client', "Invalid share form " + data.client.workerName + " ErrorCode: " + data.errorCode + " ErrorDescription: " + data.errorDescription);
+    }).on('log', function(severity, logKey, logText) {
+        if (severity == 'debug') {
+            logDebug(coinOptions.name, logKey, logText);
+        } else if (severity == 'warning') {
+            logWarning(coinOptions.name, logKey, logText);
+        } else if (severity == 'error') {
+            logError(coinOptions.name, logKey, logText);
+        }
+    });
 
 });
 
