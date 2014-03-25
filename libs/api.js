72c26213a19451b971b1a19203cad233bed52f09
Matt <zone117x@gmail.com>
2014-03-25 16:21:17 -0600

    Updated git info for node-stratum-pool and refactored website/api structure.

--- libs/api.js
+++ libs/api.js
@@ -1,1 +1,39 @@
-//create the stats object in here. then let the website use this object. that way we can have a config for stats and website separate :D
+var redis = require('redis');
+var async = require('async');
+
+var stats = require('./stats.js');
+
+module.exports = function(logger, portalConfig, poolConfigs){
+
+
+    var _this = this;
+
+    var portalStats = this.stats = new stats(logger, portalConfig, poolConfigs);
+
+    this.liveStatConnections = {};
+
+    this.handleApiRequest = function(req, res, next){
+        switch(req.params.method){
+            case 'stats':
+                res.end(portalStats.statsString);
+                return;
+            case 'live_stats':
+                res.writeHead(200, {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive'
+                });
+                res.write('\n');
+                var uid = Math.random().toString();
+                _this.liveStatConnections[uid] = res;
+                req.on("close", function() {
+                    delete _this.liveStatConnections[uid];
+                });
+
+                return;
+            default:
+                next();
+        }
+    };
+
+};
--- libs/stats.js
+++ libs/stats.js
@@ -11,7 +11,9 @@ module.exports = function(logger, portalConfig, poolConfigs){
     var redisClients = [];
 
     var algoMultipliers = {
+        'x11': Math.pow(2, 16),
         'scrypt': Math.pow(2, 16),
+        'scrypt-jane': Math.pow(2,16),
         'sha256': Math.pow(2, 32)
     };
 
@@ -38,7 +40,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
     this.statsString = '';
 
 
-    this.getStats = function(callback){
+    this.getGlobalStats = function(callback){
 
         var allCoinStats = {};
 
