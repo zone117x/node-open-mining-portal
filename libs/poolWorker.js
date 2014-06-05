96ed68290b34dcab282e5bf96940d9b71c34405a
bonesoul <shalafiraistlin@gmail.com>
2014-06-05 13:04:53 +0300

    made sure we clean extra strings from workerNames - fixed #194

--- libs/poolWorker.js
+++ libs/poolWorker.js
@@ -144,6 +144,8 @@ module.exports = function(logger){
                         }
                     }
                     else {
+                        workerName = workerName.replace(/([\-_.!~*'()].*)/g, ''); // strip any extra strings from worker name.
+                        
                         pool.daemon.cmd('validateaddress', [workerName], function (results) {
                             var isValid = results.filter(function (r) {
                                 return r.response.isvalid
