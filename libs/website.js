0ad83f5d574be2501d9df16db662bab4508cdac7
sigwo <signalwo@gmail.com>
2014-08-25 17:38:58 -0400

    Update website.js

--- libs/website.js
+++ libs/website.js
@@ -37,7 +37,6 @@ module.exports = function(logger){
         'home.html': '',
         'getting_started.html': 'getting_started',
         'stats.html': 'stats',
-        'tbs.html': 'tbs',
         'workers.html': 'workers',
         'api.html': 'api',
         'admin.html': 'admin',
