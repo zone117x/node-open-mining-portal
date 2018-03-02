a418dcd868d1d2a3c7497f13bc103fb54c53eaf7
globaltoken <globaltoken@users.noreply.github.com>
2018-03-02 07:41:06 +0100

    Updated to new Core 0.16 Format (getaddressinfo)
    Since the new Wallet release of Bitcoin Core, they introduced a new Validation format for addresses.
    Validateaddress is not longer working.

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -70,6 +70,17 @@ function SetupForPool(logger, poolOptions, setupFinished){
                     callback(true);
                 }
                 else if (!result.response || !result.response.ismine) {
+                        daemon.cmd('getaddressinfo', [poolOptions.address], function(result) {
+                    if (result.error){
+                        logger.error(logSystem, logComponent, 'Error with payment processing daemon ' + JSON.stringify(result.error));
+                        callback(true);
+                    }
+                    else if (!result.response || !result.response.ismine) {
+                        logger.error(logSystem, logComponent,
+                                'Daemon does not own pool address - payment processing can not be done with this daemon, '
+                                + JSON.stringify(result.response));
+                        callback(true);
+                    }
                     logger.error(logSystem, logComponent,
                             'Daemon does not own pool address - payment processing can not be done with this daemon, '
                             + JSON.stringify(result.response));
